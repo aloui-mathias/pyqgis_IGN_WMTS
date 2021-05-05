7ca10d9e366262eb4cdad08f0eada4bc27e0bbcd
Mathias Aloui <mathias.aloui@gmail.com>
2021-05-05 15:51:31 +0200

    simplification code

--- generate_tiff.py
+++ generate_tiff.py
@@ -57,14 +57,15 @@ def main(xmin: float, ymin: float, xmax: float, ymax: float,
     }
 
     # Parse these parameters
-    WMTS_URL_FINAL = urllib.parse.unquote(urllib.parse.urlencode(WMTS_URL_PARAMS))
+    WMTS_URL_FINAL = urllib.parse.unquote(
+        urllib.parse.urlencode(WMTS_URL_PARAMS))
 
     # Check if the layer is still available
-    WMTS_LAYER = qgis.core.QgsRasterLayer(WMTS_URL_FINAL, "raster-layer", "wms")
-    if WMTS_LAYER.isValid():
-        qgis.core.QgsProject.instance().addMapLayer(WMTS_LAYER)
-    else:
-        print(qgis_wmts_layer_manual.error().message())
+    WMTS_LAYER = qgis.core.QgsRasterLayer(
+        WMTS_URL_FINAL, "raster-layer", "wms")
+    if not WMTS_LAYER.isValid():
+        print(WMTS_LAYER.error().message())
+        exit()
 
     # Change the extent of the layer
     WMTS_LAYER.setExtent(extent)
@@ -72,14 +73,17 @@ def main(xmin: float, ymin: float, xmax: float, ymax: float,
     # Create the settings for the rederer
     settings = qgis.core.QgsMapSettings()
     settings.setLayers([WMTS_LAYER])
+
     # Default backgroud is black
     settings.setBackgroundColor(qgis.PyQt.QtGui.QColor(255, 255, 255))
-    # The size of the image is the wanted extent divided by the resoltion in meters per pixel
+
+    # The size of the image is the wanted extent divided by the resoltion in
+    # meters per pixel
     settings.setOutputSize(
         qgis.PyQt.QtCore.QSize(
-            extent.width()/resolution, extent.height()/resolution))
+            extent.width() / resolution, extent.height() / resolution))
     settings.setExtent(WMTS_LAYER.extent())
-    
+
     # Reder the image
     render = qgis.core.QgsMapRendererParallelJob(settings)
 
@@ -94,7 +98,7 @@ def main(xmin: float, ymin: float, xmax: float, ymax: float,
     loop = qgis.PyQt.QtCore.QEventLoop()
     render.finished.connect(loop.quit)
     loop.exec_()
-    
+
     # Exit the Qgis app
     QGS.exitQgis()
 
@@ -110,55 +114,55 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "xmin",
-        help = "latitude or abscissa value of the bottom left corner",
-        type = float
+        help="latitude or abscissa value of the bottom left corner",
+        type=float
     )
     parser.add_argument(
         "ymin",
-        help = "longitude or ordinate value of the bottom left corner",
-        type = float
+        help="longitude or ordinate value of the bottom left corner",
+        type=float
     )
     parser.add_argument(
         "xmax",
-        help = "latitude or abscissa value of the top right corner",
-        type = float
+        help="latitude or abscissa value of the top right corner",
+        type=float
     )
     parser.add_argument(
         "ymax",
-        help = "longitude or ordinate value of the top right corner",
-        type = float
+        help="longitude or ordinate value of the top right corner",
+        type=float
     )
     parser.add_argument(
         "--epsg",
-        help = "use to specify the espg system of the coordinates",
-        type = int
+        help="use to specify the espg system of the coordinates",
+        type=int
     )
     parser.add_argument(
         "--resolution",
-        help = "use to specify a resolution in meters per pixel (default 0.2)",
-        type = float,
-        default = 0.2
+        help="use to specify a resolution in meters per pixel (default 0.2)",
+        type=float,
+        default=0.2
     )
     parser.add_argument(
         "--time",
-        help = "use to print the execution time",
-        action = "store_true"
+        help="use to print the execution time",
+        action="store_true"
     )
     parser.add_argument(
         "--display",
-        help = "use to display the generated tif file",
-        action = "store_true"
+        help="use to display the generated tif file",
+        action="store_true"
     )
     parser.add_argument(
         "--path",
-        help = "use to specify the path for the output tif file without the extension .tif",
-        type = str,
-        default = "tile"
-    )
+        help="use to specify the path for the output tif file without the extension .tif",
+        type=str,
+        default="tile")
     args = parser.parse_args()
 
     start = time.time()
-    main(args.xmin, args.ymin, args.xmax, args.ymax, args.epsg, args.display, args.path, args.resolution)
+    main(args.xmin, args.ymin, args.xmax, args.ymax,
+         args.epsg, args.display, args.path, args.resolution)
     end = time.time()
     duration = end - start
     if args.time:
